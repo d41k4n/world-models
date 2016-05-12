5d71ec0c85397f63160b8af648400bf4e89822ee
Henry Favretto <ecm@gmx.ch>
2016-05-12 15:34:00 +0200

    Initial commit of the latest official version (0.8.0) available from http://forums.x-plane.org/index.php?/files/file/32135-world-models-library/

--- forests/Europe/mixed-shrub.for
+++ forests/Europe/mixed-shrub.for
@@ -14,13 +14,13 @@ RANDOM 3 3
 
 #tree	s		t			w		y		offset	%		height	height	quads	type	name
 
-TREE	1217	1513		529		643		275		20		3		5		2		0		tall broad 1
-TREE	37		756			548		637		229		20		3	    5		2		0		medium broad 1
-TREE	684		1400		521		630		191		10		3		5		2	    0		tall bare 1
+TREE	1217	1513		529		643		275		20		4		5		2		0		tall broad 1
+TREE	37		756			548		637		229		20		4	    5		2		0		medium broad 1
+TREE	684		1400		521		630		191		10		4		5		2	    0		tall bare 1
 
-TREE	14	    2		    526		618		106		20		3		5		2		1		scot's pine
-TREE	538	    0		    526		666		262		10		3		5		2		1		pine
-TREE    1586    0           462     786     230     20      3       5      2       1       tall pine
+TREE	14	    2		    526		618		106		20		4		5		2		1		scot's pine
+TREE	538	    0		    526		666		262		10		4		5		2		1		pine
+TREE    1586    0           462     786     230     20      4       5      2       1       tall pine
 
 
 SKIP_SURFACE water
